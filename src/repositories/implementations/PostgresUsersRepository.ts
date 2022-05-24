57537c15ff631d497fb7b1618cd9e0c21bee5826
felipeost.torres <felipeost.torres@gmail.com>
2022-05-23 22:19:15 -0300

    first commit

---
+++ src/repositories/implementations/PostgresUsersRepository.ts
@@ -0,0 +1,23 @@
+import { IUsersRepository } from "../IUsersRepository";
+import { User } from "../../entities/User";
+
+export class PostgresUsersRepository implements IUsersRepository {
+  private users: User[] = [];
+
+  async findByEmail(email: string): Promise<User> {
+    const user = this.users.find(user => user.email === email);
+
+    return user;
+  }
+
+  async save(user: User): Promise<void> {
+    this.users.push(user);
+  }
+
+  async deleteUser(id: string): Promise<void> {
+    const index = this.users.findIndex(user => user.id === id);
+    if (index > -1) {
+      this.users.splice(index, 1);
+    }
+  }
+}
